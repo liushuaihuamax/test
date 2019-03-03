26240be3ff3f3628c714566868b293b6946a01bb
Liu ShuaiHua <1457673941@qq.com>
2019-03-03 12:48:47 +0800

    程序设计2 第一部分HIJ

---
+++ test程设2H/main.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+union white
+{
+    int a;
+    double b;
+    char c[30];
+} bian[100001];
+char str[100001][30];
+int main()
+{
+    int m,n,i,j,k;
+    double f;
+    char stri[30];
+    scanf("%d %d",&n,&m);
+    for(i=0; i<n; i++)
+    {
+        scanf("%s",str[i]);
+        if(strcmp("INT",str[i])==0)
+        {
+            scanf("%d",&j);
+            bian[i].a=j;
+        }
+        else if(strcmp("DOUBLE",str[i])==0)
+        {
+            scanf("%lf",&f);
+            bian[i].b=f;
+        }
+        else if(strcmp("STRING",str[i])==0)
+        {
+            scanf("%s",stri);
+            strcpy(bian[i].c,stri);
+        }
+    }
+    for(i=0; i<m; i++)
+    {
+        scanf("%d",&k);
+        if(strcmp("INT",str[k])==0)
+        {
+            printf("%d\n",bian[k].a);
+        }
+        else if(strcmp("DOUBLE",str[k])==0)
+        {
+            printf("%.2lf\n",bian[k].b);
+        }
+        else if(strcmp("STRING",str[k])==0)
+        {
+            printf("%s\n",bian[k].c);
+        }
+    }
+    return 0;
